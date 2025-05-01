15e24913773a3c4df67d207ea994ff9904726281
yves848 <yves.godart@gmail.com>
2025-05-01 14:36:54 +0200

    avant de déployer

--- src/AquariumLightingService.pas
+++ src/AquariumLightingService.pas
@@ -3,15 +3,104 @@ program AquariumLightingService;
 {$mode objfpc}{$H+}
 
 uses
-  Classes, SysUtils, fpjson, jsonparser, fphttpclient, DateUtils;
+  Classes, SysUtils, fpjson, jsonparser, fphttpclient, DateUtils,
+  fphttpserver, HTTPDefs;
+
+type
+  TServiceMode = (smAuto,smManual);
+
+var
+  CurrentServiceMode : TServiceMode;
+  ManualState, OnTime, OffTime, LastCommand : string;
 
 const
   CONFIG_FILE = '/etc/aquarium/config.json'; // À adapter
   API_BASE_URL = 'http://192.168.50.202';      // IP ou nom DNS de ton ESP32
   SLEEP_MS = 60000; // 1 minute
 
-type
-  TServiceMode = (smAuto, smManual);
+function IfThen(ACondition: Boolean; const ATrue, AFalse: string): string;
+begin
+  if ACondition then
+    Result := ATrue
+  else
+    Result := AFalse;
+end;
+
+
+type 
+  tWebServerThread = class(TThread)
+  private
+    FServer : TFPHTTPServer;
+    procedure ServerRequest(Sender : tObject; var ARequest: TFPHTTPConnectionRequest;
+      var AResponse: TFPHTTPConnectionResponse);
+  protected
+    procedure Execute; override;
+  public
+    constructor Create;
+    destructor Destroy; override;
+  end;
+
+constructor tWebServerThread.Create;
+begin
+  inherited Create(False);
+  FreeOnTerminate := True;
+  FServer := TFPHTTPServer.Create(nil);
+  FServer.Port := 8080;
+  FServer.OnRequest := @ServerRequest;
+end;
+
+destructor tWebServerThread.Destroy;
+begin
+  if Assigned(FServer) then FServer.Free;
+  inherited Destroy;
+end;
+
+procedure tWebServerThread.Execute;
+begin
+  FServer.Active := True;
+  while not Terminated do
+    sleep(100);
+end;
+
+procedure tWebServerThread.ServerRequest(Sender : TObject; var ARequest : TFPHTTPConnectionRequest; var AResponse : TFPHTTPConnectionResponse);
+var 
+  Query : string;
+  sMode : String;
+begin
+  Query := Arequest.URL;
+  if Query = '/Status' then
+  begin
+    sMode := IfThen(CurrentServiceMode = smAuto,'auto','manual');
+    AResponse.Content := Format(
+      '{"mode": "%s", "manual_state": "%s", "on_time": "%s", "off_time":"%s", "last_command": "%s"}',[sMode,ManualState, OnTime, OffTime, LastCommand]
+    );
+    AResponse.Code := 200;
+  end
+  else if Pos('/setmode',Query) = 1 then
+  begin
+    if Pos('mode=auto', Query) > 0 then
+    begin
+      CurrentServiceMode := smAuto;
+      AResponse.Content := '{"result": "Mode réglé sur auto"}';
+    end
+    else if Pos('mode=manual', Query) > 0 then
+    begin
+      CurrentServiceMode := smManual;
+      AResponse.Content := '{"result": "Mode réglé sur manual"}';
+    end
+    else 
+      AResponse.Content := '{"error": "Mode invalide"}';
+    AResponse.Code := 200;
+  end
+  else
+  begin
+    AResponse.Code := 404;
+    AResponse.Content := '{"error": "Not Found"}';
+  end;
+end;
+
+
+
 
 procedure CallAPI(const Endpoint: String);
 var
@@ -27,17 +116,19 @@ begin
   end;
 end;
 
-procedure LoadConfig(out Mode: TServiceMode; out ManualState, OnTime, OffTime: String);
+procedure LoadConfig;
 var
-  JSON: TJSONData;
-  FileContent: String;
-  ModeStr: String;
-  Stream: TFileStream;
+  JSON : TJSONData;
+  FileContent : string;
+  ConfigFile : String;
+  ModeStr : String;
+  Stream : TFileStream;
 begin
-  if not FileExists(CONFIG_FILE) then
+  ConfigFile := '/etc/aquarium/config.json';
+  if not FileExists(ConfigFile) then
     raise Exception.Create('Config file not found');
-
-  Stream := TFileStream.Create(CONFIG_FILE, fmOpenRead);
+  
+  Stream := TFileStream.Create(ConfigFile, fmOpenRead);
   try
     SetLength(FileContent, Stream.Size);
     Stream.ReadBuffer(FileContent[1], Stream.Size);
@@ -49,12 +140,12 @@ begin
   try
     ModeStr := LowerCase(JSON.FindPath('mode').AsString);
     if ModeStr = 'manual' then
-      Mode := smManual
+      CurrentServiceMode := smManual
     else
-      Mode := smAuto;
-
+      CurrentServiceMode := smAuto;
+    
     ManualState := JSON.FindPath('manual_state').AsString;
-    OnTime := JSON.FindPath('on_time').AsString;
+    ontime := JSON.FindPath('on_time').AsString;
     OffTime := JSON.FindPath('off_time').AsString;
   finally
     JSON.Free;
@@ -63,61 +154,58 @@ end;
 
 procedure RunService;
 var
-  Mode: TServiceMode;
-  ManualState, OnTime, OffTime: String;
-  CurrentTime, LastCommand: String;
+  CurrentTime : string;
 begin
-  LastCommand := ''; // Pour éviter de répéter le même appel
-  WriteLn('[Service] Aquarium Lighting Service started.');
+  LastCommand := '';
+  WriteLn('Service Aquarium démarré.');
   while True do
   begin
     try
-      LoadConfig(Mode, ManualState, OnTime, OffTime);
-      CurrentTime := FormatDateTime('HH:NN', Now);
-
-      case Mode of
-        smManual:
+      LoadConfig;
+      CurrentTime := FormatDateTime('HH:NN',Now);
+      case CurrentServiceMode of
+        smManual :
           begin
-            if ManualState = 'day' then
+            if (ManualState = 'day') and (LastCommand <> 'day') then
             begin
-              if LastCommand <> 'day' then
-              begin
-                CallAPI('/day');
-                LastCommand := 'day';
-              end;
+              TFPHTTPClient.SimpleGet('http://192.168.50.202/day');
+              LastCommand := 'day';
+              WriteLn(Format('[%s] Mode manuel: appel /day',[FormatDateTime('hh:nn:ss',Now)]));
             end
-            else if ManualState = 'night' then
+            else if (ManualState = 'night') and (LastCommand <> 'night') then
             begin
-              if LastCommand <> 'night' then
-              begin
-                CallAPI('/night');
-                LastCommand := 'night';
-              end;
+              TFPHTTPClient.SimpleGet('http://192.168.50.202/night');
+              LastCommand := 'night';
+              Writeln(Format('[%s] Mode manuel: appel /night',[FormatDateTime('hh:nn:ss',Now)]));
             end;
           end;
-        smAuto:
+        smAuto :
           begin
-            if (CurrentTime = OnTime) and (LastCommand <> 'day') then
+            if (CurrentTime = ontime) and (LastCommand <> 'day') then
             begin
-              CallAPI('/day');
+              TFPHTTPClient.SimpleGet('http://192.168.50.202/day');
               LastCommand := 'day';
+              Writeln(Format('[%s] Mode auto: appel /day',[FormatDateTime('hh:nn:ss',now)]));
             end
             else if (CurrentTime = OffTime) and (LastCommand <> 'night') then
             begin
-              CallAPI('/night');
+              TFPHTTPClient.SimpleGet('http://192.168.50.202/night');
               LastCommand := 'night';
+              Writeln(Format('[%s] Mode auto: appel /night',[FormatDateTime('hh:nn:ss',now)]));
             end;
           end;
       end;
     except
-      on E: Exception do
-        WriteLn(Format('[%s] Error: %s', [FormatDateTime('hh:nn:ss', Now), E.Message]));
+      on e: exception do
+        Writeln(format('[%s] Erreur: %s',[FormatDateTime('hh:nn:ss',now), E.Message]));
     end;
-
-    Sleep(SLEEP_MS);
+    sleep(60000);
   end;
 end;
 
 begin
+  with tWebServerThread.create do
+    Writeln('Serveur HTTP démarré sur le port 8080.');
+
   RunService;
 end.
