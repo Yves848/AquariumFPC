5d8c0e0f63119a478c3c55f24b09bc8652a4c563
yves848 <yves.godart@gmail.com>
2025-06-28 18:05:12 +0200

    WIP

--- src/AquariumLightingService.pas
+++ src/AquariumLightingService.pas
@@ -307,7 +307,7 @@ Begin
               writeln(Format('[%s] Heure d''extinction : %s',[FormatDateTime('hh:nn:ss',Now), FormatDateTime('hh:nn:ss',offtime)]));
               writeln(Format('[%s] État actuel : %s',[FormatDateTime('hh:nn:ss',Now), state]));
               // Convert ontime and of
-              If (CurrentTime >= ontime and CurrentTime < offtime>) And (state <> 'day') Then
+              If ((CurrentTime >= ontime) and (CurrentTime < offtime)) And (state <> 'day') Then
               Begin
                 SetLightingMode('day');
                 LastCommand := 'day';
